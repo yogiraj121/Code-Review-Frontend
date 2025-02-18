5bdee2e616994b16638e7080064494f1bf43b997
Yogiraj Mahale <yogirajmahale566@gmail.com>
2025-02-18 10:40:04 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-review-backend-u3tk.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
